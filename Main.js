fa6b2285bee6f185ce7520c80d6ea1b52612deee
Nyckollas Giampaoli <122179728+NyckollasGiampaoli@users.noreply.github.com>
2023-10-23 21:30:47 -0300

    Main.js

--- Main.js
+++ Main.js
@@ -1,3 +1,9 @@
+function Pessoal(){
+    str.link("https://nyckollasgiampaoli.github.io/PortfolioNyckollas.github.io/")
+}
+function Profissional(){
+    str.link("file:///D:/UNINTER/Desafio-Faculdade/perfil.html")
+}
 function Contatos(){
-    window.alert("Teste");
+    window.alert("Twitter: Nyck_4_Giamp \n Whatsapp: 98064-0882 \n Link do Github: https://github.com/NyckollasGiampaoli");
 };
